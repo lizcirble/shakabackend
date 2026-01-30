a9ffa3036f517688672f3da6e110f5f163d8179f
Clement Raymond <chinexzy37@gmail.com>
2026-01-30 23:45:25 +0000

    feat: implement compute share update flow

--- dataRand_front-end/app/compute/history/page.tsx
+++ dataRand_front-end/app/compute/history/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ComputeHistory } from "@/pages/ComputeHistory";
+import ComputeHistory from "@/pages/ComputeHistory";
 
 export default function ComputeHistoryPage() {
   return <ComputeHistory />;
