d9b26352bd699be9be3680c3de2c7146625844b5
Clement Raymond <chinexzy37@gmail.com>
2026-01-31 00:11:10 +0000

    feat: implement compute share update flow

--- dataRand_front-end/app/client/tasks/[id]/page.tsx
+++ dataRand_front-end/app/client/tasks/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TaskDetail } from "@/pages/client/TaskDetail";
+import TaskDetail from "@/pages/client/TaskDetail";
 
 export default function TaskDetailPage({ params }: { params: { id: string } }) {
   return <TaskDetail taskId={params.id} />;
